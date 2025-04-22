c9b211e700b545d378b10d21969bb4a77b21f261
Jesslyn <z5455953@ad.unsw.edu.au>
2025-04-22 23:10:34 +1000

    Fixed the polling to also show results when the game is over

--- frontend/src/Players/PlayerPlayScreen.jsx
+++ frontend/src/Players/PlayerPlayScreen.jsx
@@ -6,45 +6,61 @@ import "./PlayerPlayScreen.css";
 
 const PlayerPlayScreen = () => {
   const [question, setQuestion] = useState(null);
+  const [results, setResults] = useState(null);
 
   const { playerId } = useParams();
 
   const getQuestion = async () => {
     try {
-      const token = localStorage.getItem('token');
-      const response = await axios.get(`http://localhost:5005/play/${playerId}/question`, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        }
-      });
+      const response = await axios.get(`http://localhost:5005/play/${playerId}/question`);
       console.log(response.data);
       setQuestion(response.data.question);
     } catch (err) {
-      console.log(err);
+      console.log(err.response?.data?.error);
       setQuestion(null);
     }
   }
 
+  const getResults = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5005/play/${playerId}/results`);
+      console.log("HERE")
+      console.log(response.data)
+      setResults(response.data);
+    } catch (err) {
+      console.log(err.response?.data?.error);
+      setResults(null);
+    }
+  }
+
   useEffect(() => {
-    setInterval(getQuestion, 1000);
-  });
+    setInterval(() => {
+      getQuestion();
+      getResults();
+    }, 2000);
+  }, []);
 
   return (
     <>
-      {!question && (
+      {!question && !results && (
         <video autoPlay loop muted className="background-video">
           <source src="/Grass.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       )}
-      {question ? 
+      {question && !results ? (
         <div>
           {question.question}
-        </div> :
-        <div class="general-style" className={!question ? "overlay-content" : ""}>
+        </div>
+      ) : !question && results ? (
+        <div>
+          results of the game
+        </div>
+      ) : (
+        <div className="general-style overlay-content">
           <h1 id="player-wait-text">Please wait for the game to start...</h1>
         </div>
-      }
+      )}
     </>
   )
 }

033bc2af935e0f84f8167539fe23ea8120d07cbb
Jesslyn <z5455953@ad.unsw.edu.au>
2025-04-25 05:35:28 +1000

    Added tests and made some updates/fixes

--- frontend/src/RegisterLogin/RegisterLogin.jsx
+++ frontend/src/RegisterLogin/RegisterLogin.jsx
@@ -53,48 +53,52 @@ const RegisterLogin = ({ successJob, token, name}) => {
         <Form onSubmit={(e) => { e.preventDefault(); tryLoginRegister(); }}>
           <h1 >{capitalize(name)}</h1><br/>
           <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon1">Email </InputGroup.Text>
+            <InputGroup.Text id="email-input-title">Email </InputGroup.Text>
             <Form.Control value={email} onChange={e => setEmail(e.target.value)}
               placeholder="123@email.com"
               aria-label="Email"
               type="email"
-              aria-describedby="basic-addon1"
+              aria-describedby="email-input-title"
+              id="email-input"
             />
           </InputGroup>
           <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon1">Password </InputGroup.Text>
+            <InputGroup.Text id="password-input-title">Password </InputGroup.Text>
             <Form.Control value={password} onChange={e => setPassword(e.target.value)}
               placeholder="Password123"
               aria-label="Password"
-              aria-describedby="basic-addon1"
+              aria-describedby="password-input-title"
               type="password"
+              id="password-input"
             />
           </InputGroup>
 
           {name === 'register' && (
             <>
               <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">Username</InputGroup.Text>
+                <InputGroup.Text id="username-input-title">Username</InputGroup.Text>
                 <Form.Control value={userName} onChange={e => setUserName(e.target.value)}
                   placeholder="Your display name"
                   aria-label="Username"
                   type="username"
-                  aria-describedby="basic-addon1"
+                  aria-describedby="username-input-title"
+                  id="username-input"
                 />
               </InputGroup>
 
               <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">Confirm Password</InputGroup.Text>
+                <InputGroup.Text id="confirm-password-input-title">Confirm Password</InputGroup.Text>
                 <Form.Control value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}
                   placeholder="Confirm password"
                   aria-label="Confirm password"
-                  aria-describedby="basic-addon1"
+                  aria-describedby="confirm-password-input-title"
                   type="password"
+                  id="confirm-password-input"
                 />
               </InputGroup>
             </>
           )}
-          <Button type="submit" onClick={tryLoginRegister} variant="primary">Submit</Button>
+          <Button id="submit-login-register-form" type="submit" onClick={tryLoginRegister} variant="primary">Submit</Button>
         </Form>
       </div>
 
